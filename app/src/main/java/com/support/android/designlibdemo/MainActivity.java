3d1bca2765a2f4ae963a1f94d6c1c31eb1130841
Marc <marcsalvat@gmail.com>
2016-07-11 23:47:57 +0200

    Sample app

---
+++ app/src/main/java/com/support/android/designlibdemo/MainActivity.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.support.android.designlibdemo;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    static
+    {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    private DrawerLayout mDrawerLayout;
+    ActionMenuView amv;
+    Toolbar toolbar;
+    Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null)
+        {
+            setupDrawerContent(navigationView);
+        }
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null)
+        {
+            setupViewPager(viewPager);
+        }
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                CheeseListFragment fragment = (CheeseListFragment) adapter.instantiateItem(viewPager, 0);
+                fragment.updateListItems();
+                Snackbar.make(view, "notifyDataSetChanged() was called in recycler adapter", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        final Animation fadeIn = AnimationUtils.loadAnimation(this, android.support.v7.appcompat.R.anim.abc_fade_out);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                amv.getChildAt(amv.getChildCount() - 1).startAnimation(fadeIn);
+            }
+        }, 4000);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        getDelegate().applyDayNight();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        for (int i = 0; i < toolbar.getChildCount(); ++i)
+        {
+            if (toolbar.getChildAt(i).getClass().getSimpleName().equals("ActionMenuView"))
+            {
+                amv = (ActionMenuView) toolbar.getChildAt(i);
+                break;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode)
+        {
+            case Configuration.UI_MODE_NIGHT_NO:
+                Log.i("MainActivity", "Night mode is not active, we're in day time");
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                Log.i("MainActivity", "Night mode is active, we're at night!");
+                break;
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                Log.i("MainActivity", "We don't know what mode we're in, assume notnight");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new CheeseListFragment(), "Category 1");
+        adapter.addFragment(new CheeseListFragment(), "Category 2");
+        adapter.addFragment(new CheeseListFragment(), "Category 3");
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.requestTransparentRegion(viewPager);
+        viewPager.setCurrentItem(1);
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView)
+    {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                adapter.addFragment(new CheeseListFragment(), "Category 4");
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    static class Adapter extends FragmentPagerAdapter
+    {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title)
+        {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount()
+        {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return mFragmentTitles.get(position);
+        }
+    }
+}
